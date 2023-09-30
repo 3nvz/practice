cb4a856869895cf95d849889ed3833bff4427705
3nvz <rscherer.jobs@gmail.com>
2023-09-30 09:29:59 -0700

    prompt for comma-separated numbers, calculate the rounded-down square roots, and print the results as a comma-separated string.

--- practice.py
+++ practice.py
@@ -1,20 +1,18 @@
+import math
 
+# 6
+# input into array
+# loop through array and perform math ops
+# append to array and print
 
-#Class with 2 Methods
-#Create Test cases
+def mathOperation():
+    result = []
+    items = [int(x) for x in input("Give me comma seperated numbers: ").split(",")]
+    print(items)
+    for d in items:
+        result.append(str(math.floor(math.sqrt((2 * 50 * d)/30))))
+    
+    commaSepOutput = ','.join(result)
+    print(commaSepOutput)
 
-class TestClass:
-
-
-    def __init__(self):
-        pass
-
-    def getString(self):
-        self.s = input("Give me sth to say: ")
-        
-    def printString(self):
-        print(self.s)
-
-test = TestClass()
-test.getString()
-test.printString()
+mathOperation()

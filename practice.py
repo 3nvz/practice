082dd7f9ca4538e07c1532e43615740dcf90ab42
3nvz <rscherer.jobs@gmail.com>
2023-10-03 22:35:00 -0700

    improved wording to calculate lower/upper case characters in a sentence

--- practice.py
+++ practice.py
@@ -8,11 +8,11 @@ sentence = input("Give me a sentence: ")
 countUpper = 0
 countLower = 0
 
-for letter in sentence:
-    if letter.isupper():
+for character in sentence:
+    if character.isupper():
         countUpper += 1
-    elif letter.islower():
+    elif character.islower():
         countLower += 1
 
-print("Number of upper case letter: ", countUpper)
-print("Number of lower case letter: ", countLower)
+print("Number of upper case characters: ", countUpper)
+print("Number of lower case characters: ", countLower)

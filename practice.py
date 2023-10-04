52c8943b05cf01ed12816fca81691d600d7824a5
3nvz <rscherer.jobs@gmail.com>
2023-10-03 22:10:05 -0700

    count letters and digits in a sentence

--- practice.py
+++ practice.py
@@ -1,14 +1,19 @@
 import math
 
-# 12
-# take numbers from range 2000 to 3000
-# print nums where all digits are even
+# 13
+# accepts a setence with letters and digits
+# calculate number of letters and digits
 
-results = []
+sentence = [x for x in input("Give me a sentence: ").split()]
+countLetter = 0
+countDigit = 0
 
-for i in range(2000, 3001):
-    s = str(i)
-    if int(s[0]) % 2 == 0 and int(s[1]) % 2 == 0 and int(s[2]) % 2 == 0 and int(s[3]) % 2 == 0:
-        results.append(s)
+for word in sentence:
+    for i in range(0, len(word)):
+        if word[i].isalpha():
+            countLetter += 1
+        elif word[i].isdigit():
+            countDigit += 1
 
-print(','.join(results))
+print("Number of letters: ", countLetter)
+print("Number of digits: ", countDigit)

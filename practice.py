a7ae2f37040a91593fb5d8626c519db1884c16fe
3nvz <rscherer.jobs@gmail.com>
2023-10-05 05:34:27 -0700

    small bank transaction logics not considering negative amounts/no inputs

--- practice.py
+++ practice.py
@@ -1,13 +1,23 @@
 import math
 
-# 16
-# comma seperated input of ints saved in a list
-# output all odd ints comma seperated
+# 17
+# bank account transactions
+# where D stands for deposit and W for withdrawal
 
-numbers = [int(x) for x in input("Give me comma seperated numbers: ").split(",")]
-results = []
+moneyAmount = 0
 
-for num in numbers:
-    if num % 2 != 0:
-        results.append(str(num))
-print(','.join(results))
+while True:
+    inp = input("Do you want to deposit or withdraw? ")
+    values = inp.split(" ")
+
+    op = values[0]
+    amount = int(values[1])
+
+    if op == 'D':
+        moneyAmount += amount
+    elif op == 'W':
+        moneyAmount -= amount
+    elif op == 'E':
+        break
+    
+    print("Amount in the bank: ", moneyAmount)

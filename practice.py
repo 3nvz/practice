7f586416f5ce7d8f120d520fbb5d53d8c42a27dd
3nvz <rscherer.jobs@gmail.com>
2023-10-06 12:21:15 -0700

    Password Policy - input comma sep passwords, check against multiple functions if password policy meets

--- practice.py
+++ practice.py
@@ -1,6 +1,30 @@
 import math
 
-# 24
-# print documentation of functions
+# 18
+# check a password for various factors
+# put some password policy in place
 
-print(abs.__doc__)
+def contains_digits(pwd):
+    return any(char.isdigit() for char in pwd)
+
+def contains_lowercase(pwd):
+    return any(char.isalpha() for char in pwd)
+
+def contains_upperCase(pwd):
+    return any(char.isupper() for char in pwd)
+
+def contains_special(pwd):
+    return '@' in pwd or '#' in pwd or '$' in pwd
+
+
+passwords = [x for x in input("Give me some passwords: ").split(",")]
+
+
+for pwd in passwords:
+    if len(pwd) > 6 and len(pwd) <= 12:
+        if contains_digits(pwd) and contains_lowercase(pwd) and contains_upperCase(pwd) and contains_special(pwd):
+            print(f"The password '{pwd}' meets the requirements.")
+        else:
+            print(f"The password '{pwd}' doesn't meet the requirements.")
+    else:
+        print(f"The password '{pwd}' is not long enough.")

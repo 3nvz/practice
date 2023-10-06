04167fcd0f865872f0a04562ef2e255fdc4f62a7
3nvz <rscherer.jobs@gmail.com>
2023-10-06 05:03:46 -0700

    print the docs of a built in function

--- practice.py
+++ practice.py
@@ -1,23 +1,6 @@
 import math
 
-# 17
-# bank account transactions
-# where D stands for deposit and W for withdrawal
+# 24
+# print documentation of functions
 
-moneyAmount = 0
-
-while True:
-    inp = input("Do you want to deposit or withdraw? ")
-    values = inp.split(" ")
-
-    op = values[0]
-    amount = int(values[1])
-
-    if op == 'D':
-        moneyAmount += amount
-    elif op == 'W':
-        moneyAmount -= amount
-    elif op == 'E':
-        break
-    
-    print("Amount in the bank: ", moneyAmount)
+print(abs.__doc__)

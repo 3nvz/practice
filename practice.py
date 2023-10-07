a7f69056cd983cc9094cd07ebf289c67a91fcb52
3nvz <rscherer.jobs@gmail.com>
2023-10-07 08:06:43 -0700

    calculate the square number of an int

--- practice.py
+++ practice.py
@@ -1,30 +1,11 @@
 import math
 
-# 18
-# check a password for various factors
-# put some password policy in place
+# 23
+# Method to calculate the square number of an int
 
-def contains_digits(pwd):
-    return any(char.isdigit() for char in pwd)
+def square(num):
+    return num**2
 
-def contains_lowercase(pwd):
-    return any(char.isalpha() for char in pwd)
-
-def contains_upperCase(pwd):
-    return any(char.isupper() for char in pwd)
-
-def contains_special(pwd):
-    return '@' in pwd or '#' in pwd or '$' in pwd
-
-
-passwords = [x for x in input("Give me some passwords: ").split(",")]
-
-
-for pwd in passwords:
-    if len(pwd) > 6 and len(pwd) <= 12:
-        if contains_digits(pwd) and contains_lowercase(pwd) and contains_upperCase(pwd) and contains_special(pwd):
-            print(f"The password '{pwd}' meets the requirements.")
-        else:
-            print(f"The password '{pwd}' doesn't meet the requirements.")
-    else:
-        print(f"The password '{pwd}' is not long enough.")
+print(square(2))
+print(square(4))
+print(square(5))

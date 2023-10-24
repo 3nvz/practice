29a555994f14860e4293a6ba48999a0eb01ceddc
3nvz <rscherer.jobs@gmail.com>
2023-10-24 09:03:13 -0700

    Random 1

--- practice.py
+++ practice.py
@@ -3,7 +3,7 @@ import random
 
 # a function to concatinate strings
 
-def concatStrings(s1, s2):
-    print(s1 + " " + s2)
-
-concatStrings("I got", "CONCATINATED!")
+x = 2
+y = 7
+z = True
+print(x, y, z)

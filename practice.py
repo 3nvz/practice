4d9df39279d83fde9877b5b2b9622659f5032095
3nvz <rscherer.jobs@gmail.com>
2023-10-25 09:50:27 -0700

    String comparisions to output all True

--- practice.py
+++ practice.py
@@ -1,7 +1,19 @@
 
-y = "lz"
+w = "b"  # <- Change this
+x = "hello"  # <- Change this
+y = "hello"  # <- Change this
+z = "d"  # <- Change this
 
-# a function to concatinate strings
-x = "hello" < "he" + y
+# Don't change any of these `condition_` variables.
+condition_1 = w != "a"
+condition_2 = w < x
+condition_3 = x == y
+condition_4 = y == "hello"
+condition_5 = z > "c"
 
-print(x)
+# All of these should print `True`.
+print(condition_1)
+print(condition_2)
+print(condition_3)
+print(condition_4)
+print(condition_5)

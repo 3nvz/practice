2781432ec796a7e5c2806f28c350a2649992f1ad
3nvz <rscherer.jobs@gmail.com>
2023-10-30 02:30:10 -0700

    String test

--- practice.py
+++ practice.py
@@ -1,19 +1,4 @@
 
-w = "b"  # <- Change this
-x = "hello"  # <- Change this
-y = "hello"  # <- Change this
-z = "d"  # <- Change this
+var = "algoexpert".upper().replace("o", "i").capitalize().lower().split("e")
 
-# Don't change any of these `condition_` variables.
-condition_1 = w != "a"
-condition_2 = w < x
-condition_3 = x == y
-condition_4 = y == "hello"
-condition_5 = z > "c"
-
-# All of these should print `True`.
-print(condition_1)
-print(condition_2)
-print(condition_3)
-print(condition_4)
-print(condition_5)
+print("foo".join(var))

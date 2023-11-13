5f18c1ea74fda5928ef5549123cbc665b6bf6806
3nvz <rscherer.jobs@gmail.com>
2023-11-13 09:47:20 -0800

    OO coding test, class and instances

--- practice.py
+++ practice.py
@@ -1,13 +1,10 @@
-class Foo:
-    def __init__(self, name):
-        self.name = name
-    
-a = Foo("a")
-b = Foo("b")
-a.name = b.name
-b.name = "c"
-a.x = 2
-b.x = 1
-x = (a.x + b.x) * (a.name + b.name)
+class ContactInformation:
+    def __init__(self, first_name, last_name, email, phone_number):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.phone_number = phone_number
+        self.country = None
 
-print(x)
+person1 = ContactInformation("John", "Doe", "test@gmail.com", "123456789")
+person2 = ContactInformation("Jane", "Doe", "test2@gmail.com", "987654321")

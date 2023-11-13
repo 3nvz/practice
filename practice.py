9a8f32c5e768b58c08af9251f82e51844c5e920d
3nvz <rscherer.jobs@gmail.com>
2023-11-13 09:43:26 -0800

    OO coding tests

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
-#Object oriented programming practice
-object oriented programming practice
+class Foo:
+    def __init__(self, name):
+        self.name = name
+    
+a = Foo("a")
+b = Foo("b")
+a.name = b.name
+b.name = "c"
+a.x = 2
+b.x = 1
+x = (a.x + b.x) * (a.name + b.name)
+
+print(x)

d620c189994f15e2f71a5b938f2bd453176a6b3e
3nvz <rscherer.jobs@gmail.com>
2023-11-17 08:03:43 -0800

    Property exercise with getters and setter and priv variables

--- practice.py
+++ practice.py
@@ -3,17 +3,20 @@ class BankAccount:
         self.account_holder_name = account_holder_name
         self._balance = 0
 
-    def set_balance(self, balance):
+
+    @property
+    def balance(self):
+        return self._balance
+
+    @balance.setter
+    def balance(self, balance):
         if isinstance(balance, int) and balance > 0 and balance < 100000:
             self._balance = balance
         else:
             raise Exception("Balance must be positive")
     
-    def get_balance(self):
-        return self._balance
-    
 
 bA1 = BankAccount("John")
-bA1.set_balance(-100)
-print(bA1.get_balance())
+bA1.balance = 100
+print(bA1.balance)
 

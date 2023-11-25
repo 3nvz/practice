65dc166a5eb8ff6788e9fa97680edea00cb303f4
3nvz <rscherer.jobs@gmail.com>
2023-11-25 11:11:06 -0800

    inheritance exercise

--- practice.py
+++ practice.py
@@ -1,24 +1,33 @@
-class Physics:
-    # Write your code here
+class Animal:
+    def __init__(self, age, weight, height):
+        self.age = age
+        self.weight = weight
+        self.height = height
 
-    @staticmethod
-    def calculate_net_force(mass, acceleration):
-        if mass < 0 or acceleration < 0:
-            return 0.0
-        
-        return mass * acceleration
 
-    @staticmethod
-    def calculate_acceleration(mass, net_force):
-        if mass <= 0 or net_force < 0:
-            return 0.0
-        
-        return net_force / mass
-        
+class Mammal(Animal):
+    def __init__(self, age, weight, height, sex):
+        super().__init__(age, weight, height)
+        self.sex = sex
 
-    @staticmethod
-    def calculate_mass(net_force, acceleration):
-        if acceleration <= 0 or net_force < 0:
-            return 0.0
-        
-        return net_force / acceleration
+
+class Reptile(Animal):
+    def __init__(self, age, weight, height, legs):
+        super().__init__(age, weight, height)
+        self.legs = legs
+
+
+class Monkey(Mammal):
+    fingers = 5
+
+    def __init__(self, age, weight, height, sex, color):
+        super().__init__(age, weight, height, sex)
+        self.color = color
+
+
+class Lizard(Reptile):
+    tail = True
+
+    def __init__(self, age, weight, height, legs, color):
+        super().__init__(age, weight, height, legs)
+        self.color = color

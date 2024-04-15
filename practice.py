30b9d18fb1c512a7fb58730ac0021d9a77d67d79
3nvz <rscherer.jobs@gmail.com>
2024-04-15 15:04:09 -0700

    2D Hourglass

--- practice.py
+++ practice.py
@@ -1,6 +1,11 @@
-
-from collections import Counter
-
-arr = [1, 3, 5, 3, 7, 9, 3]
-
-print(arr.count(3))
+def hourglassSum(arr):
+    maxSum = -5000
+    for i in range(0, 4):
+        for j in range(0, 4):
+            currentSum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2])
+            if currentSum > maxSum:
+                maxSum = currentSum
+            else:
+                continue
+            
+    return maxSum

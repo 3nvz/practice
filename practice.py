64ed1d7d82933468b985fdf27d5ab5663309b4a1
3nvz <rscherer.jobs@gmail.com>
2024-04-16 07:09:54 -0700

    nested list second lowest value

--- practice.py
+++ practice.py
@@ -1,11 +1,20 @@
-def hourglassSum(arr):
-    maxSum = -5000
-    for i in range(0, 4):
-        for j in range(0, 4):
-            currentSum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2])
-            if currentSum > maxSum:
-                maxSum = currentSum
-            else:
-                continue
-            
-    return maxSum
+
+nested_list = [['blue', 3], ['red', 3], ['blue', 4], ['blue', 7], ['blue', 6], ['blue', 1], ['blue', 5]]
+
+scores = set()
+result = []
+
+for i in range(len(nested_list)):
+    scores.add(nested_list[i][1])
+secondLowestScore = sorted(scores)[1]
+
+
+for j in range(len(nested_list)):
+    if nested_list[j][1] == secondLowestScore:
+        result.append(nested_list[j])
+    
+result.sort()
+print("Result", result)
+
+
+print(scores)

f4982e9a4baa2f40b42e10f7aa8b9ca84bac811b
3nvz <rscherer.jobs@gmail.com>
2024-04-18 10:47:05 -0700

    this is too easy to commit

--- practice.py
+++ practice.py
@@ -1,16 +1,6 @@
 
-grades = {
-    "Krishna": [67, 68, 69],
-    "Arjun": [70, 98, 63],
-    "Malika": [52, 56, 60]
-}
-
-student = grades.get("Malika")
-gradesAdded, averageGrade = 0, 0
-
-for grade in student:
-    gradesAdded += grade
-
-averageGrade = gradesAdded/len(student)
-
-print("{:.2f}".format(averageGrade))
+def aVeryBigSum(ar):
+    res = 0
+    for num in ar:
+        res += num
+    return res

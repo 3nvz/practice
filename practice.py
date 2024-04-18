88e1670228bc394e5ed57fdfda737b617c7c011e
3nvz <rscherer.jobs@gmail.com>
2024-04-18 11:09:06 -0700

    Get diagonal

--- practice.py
+++ practice.py
@@ -1,6 +1,14 @@
 
-def aVeryBigSum(ar):
-    res = 0
-    for num in ar:
-        res += num
-    return res
+arr = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+sumi = 0
+
+for i in range(len(arr)):
+    sumi += arr[i][-i-1]
+    print(arr[i][-i-1])
+
+print(sumi)
